69a72e1074cf6e10bf58c2475eaffe8ba944e8ee
Bietola <dincio.montesi@gmail.com>
2024-09-24 13:57:08 +0200

    WIP

--- chall/poli/old/CTF/serialization_CryptoPuppy/src/img.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'data.php';
-
-$b64puppy = $_GET['puppy'];
-// decode
-$puppy = base64_decode($b64puppy);
-$puppy = unserialize($puppy);
-$puppy->getImg();
-?>
--- chall/poli/old/CTF/serialization_CryptoPuppy/src/puppy.js
+++
@@ -1,9 +0,0 @@
-// set the new image on modify
-function modifyPuppy() {
-  var puppy = document.getElementById("puppy");
-  var name = document.getElementById("name").value;
-  puppy.src = "/img.php?puppy=" + name;
-}
-
-// set the event on form edit
-document.getElementById("name").addEventListener("keyup", modifyPuppy);    
